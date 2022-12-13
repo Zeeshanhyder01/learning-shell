1439280df5a5607a361d8abea79e0af5c58ae2d5
Zeeshuzeeshu <zeeshuzeeshu110@gmail.com>
2022-12-13 15:40:28 +0530

    printing

--- 02-Print.sh
+++ 02-Print.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # the command is user for printing the statements is echo
+
 #syntax : echo Input
 echo " Hello Zeeshan"
 
@@ -8,3 +9,8 @@ echo " Hello Zeeshan"
 # Print line by line
 # syntax: echo -e "Input1,\n input2"
 echo -e " zeeshan, \n Hasan, \n Zaheda"
+
+# Print Tab spac b/w two charcters
+#syntax : echo -e "a\tb"
+
+echo -e" zeeshan\tHyder"
