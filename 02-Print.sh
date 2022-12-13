9cd8e841d27c4ca5f1fa87a3b17f2dfae13982aa
Zeeshuzeeshu <zeeshuzeeshu110@gmail.com>
2022-12-13 15:36:30 +0530

    printing

--- 02-Print.sh
+++ 02-Print.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 # the command is user for printing the statements is echo
+#syntax : echo Input
 echo " Hello Zeeshan"
 
 
 # Print line by line
-
+# syntax: echo -e "Input1,\n input2"
 echo -e " zeeshan, \n Hasan, \n Zaheda"
