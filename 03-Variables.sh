24a27338e7d7a1efbe993b4729955bd00a5bd8b2
Zeeshuzeeshu <zeeshuzeeshu110@gmail.com>
2022-12-15 11:37:33 +0530

    VARIABLES DEMO

---
+++ 03-Variables.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+#Variables Decleration
+#syntax var = data
+#Access variable : $var / ${var}
+#two  types of Decleration 1-Commands 2-Arithemetic expressions
+DATE=$(date=%F)
+
+echo welcome, Today date is : $DATE
