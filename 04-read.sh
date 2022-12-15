00bb8f0041a3753881e72276ca7ef96f637930e5
Zeeshuzeeshu <zeeshuzeeshu110@gmail.com>
2022-12-15 15:10:41 +0530

    read command demo

--- 04-read.sh
+++ 04-read.sh
@@ -2,3 +2,11 @@
 
 read -p 'enter your name :' name
 echo -e "hey $name, Welcome to DevOps Training"
+
+
+read -p 'Enter Your Age :' Age
+echo -e " hey Your was $Age years old "
+
+readd -p ' Contact Number :' contact
+echo -e " Your Contact Number is : $contact, Thanks for sharing Your Details"
+echo -e " Regards \n Zeeshan"
