a2e1ae1ab4bae42f14d9b51318cca45e364eb85e
Zeeshuzeeshu <zeeshuzeeshu110@gmail.com>
2023-01-06 18:36:55 +0530

    case st

---
+++ 09-loop.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+#Fundemental loops : While , For ( Until ,Select)
+#syntax :
+#Loop-command Expression or inputs ; do
+   #Commands
+#done
+
+i=0
+while [ $i -lt 5 ]; do
+  echo Zeeshan -$i
+  i=&((i+1))
+done
+
