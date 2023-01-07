40bdafce64b2fb280bf45ec7a793c7a9454701ad
Zeeshuzeeshu <zeeshuzeeshu110@gmail.com>
2023-01-07 18:49:35 +0530

    while loop

--- 09-loop.sh
+++ 09-loop.sh
@@ -5,6 +5,8 @@
 #Loop-command Expression or inputs ; do
    #Commands
 #done
+#while uses Expressions
+#For uses Inputs
 
 i=0
 while [ $i -lt 5 ]; do
@@ -12,3 +14,8 @@ while [ $i -lt 5 ]; do
   i=$(($i+1))
 done
 
+i =10
+while [ $i -gt 10 ]; do
+  echo hyder - $i
+  i= $(($i+1))
+done
